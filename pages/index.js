6f9469992de7ddffe6dc9c3d89f9879b85752283
unknown <noahnosaaghedo@gmail.com>
2023-02-27 12:13:43 +0100

    mmm

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import Head from 'next/head'
-import Header from '../src/components/header/header';
+import Header from '@/src/components/header/header'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+
+
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({ data }) { 
   return (
