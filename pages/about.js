b4ee7b70cc51872f6646ec600ce7d7ef2fc6b174
unknown <noahnosaaghedo@gmail.com>
2023-03-28 20:49:40 +0100

    Updated meta content

--- pages/about.js
+++ pages/about.js
@@ -33,8 +33,45 @@ export default function About({ data, services }) {
         <title>Hannah Aghedo - About</title>
         <meta
           name="description"
-          content="Hannah Aghedo - An Outstanding Legal Firm"
+          content="Hannah Aghedo | An Outstanding Legal Practitioner"
         />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          name="title"
+          content="An Outstanding Legal Practitioner | Attorney Extra-Ordinaire"
+        />
+        <meta
+          name="keywords"
+          content="legal services, law firm, attorney, lawyer, legal advice, litigation, dispute resolution, contract law, business law, corporate law, employment law, labor law, civil litigation, criminal law, family law, estate planning, real estate law, immigration law, intellectual property law, mediation and arbitration"
+        />
+        <meta
+          property="og:title"
+          content="Hannah Aghedo | An Outstanding Legal Practitioner"
+        />
+        <meta property="og:url" content="https://hannahaghedo.com/#about" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://www.hannahaghedo.com/images/general/hannah-picture.jpg"
+        ></meta>
+        <meta
+          property="og:description"
+          content="Hannah Aghedo is an experienced and dedicated lawyer who is committed to providing high-quality legal services to her clients. With 10+ years of experience in several areas of law, she has a proven track record of success in handling complex legal matters. Hannah Aghedo takes a personalized approach to every case, working closely with her clients to understand their unique needs and goals. Whether you are facing a legal dispute or need guidance on a business or personal matter, Hannah Aghedo is here to help you achieve the best possible outcome."
+        ></meta>
+        <meta property="og:locale" content="en_US"></meta>
+        <meta
+          property="twitter:title"
+          content="Hannah Aghedo | Outstanding Legal Practitioner"
+        ></meta>
+        <meta
+          property="twitter:description"
+          content="Hannah Aghedo is an experienced and dedicated lawyer who is committed to providing high-quality legal services to her clients. With 10+ years of experience in several areas of law, she has a proven track record of success in handling complex legal matters. Hannah Aghedo takes a personalized approach to every case, working closely with her clients to understand their unique needs and goals. Whether you are facing a legal dispute or need guidance on a business or personal matter, Hannah Aghedo is here to help you achieve the best possible outcome."
+        ></meta>
+        <meta
+          property="twitter:image"
+          content="https://www.hannahaghedo.com/images/general/hannah-picture.jpg"
+        ></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           rel="apple-touch-icon"
--- pages/contact.js
+++ pages/contact.js
@@ -22,37 +22,35 @@ export default function Contact({ data, services }) {
   const [message, setMessage] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Sending");
 
-    let data = {
-      fname,
-      lname,
-      email,
-      phone,
-      message,
-    };
-
-    fetch("/api/contact", {
-      method: "POST",
+    const res = await fetch("/api/sendgrid", {
+      body: JSON.stringify({
+        email: email,
+        fname: fname,
+        lname: lname,
+        phone: phone,
+        message: message,
+      }),
       headers: {
-        Accept: "application/json, text/plain, */*",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data),
-    }).then((res) => {
-      console.log("Response received");
-      if (res.status === 200) {
-        console.log("Response succeeded!");
-        setSubmitted(true);
-        setFname("");
-        setLname("");
-        setEmail("");
-        setPhone("");
-        setMessage("");
-      }
+      method: "POST",
     });
+
+    const { error } = await res.json();
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log("Response succeeded!");
+    setSubmitted(true);
+    setFname("");
+    setLname("");
+    setEmail("");
+    setPhone("");
+    setMessage("");
   };
 
   const handleClick = () => {
@@ -65,8 +63,45 @@ export default function Contact({ data, services }) {
         <title>Hannah Aghedo - Contact</title>
         <meta
           name="description"
-          content="Hannah Aghedo - An Outstanding Legal Firm"
+          content="Hannah Aghedo | An Outstanding Legal Practitioner"
         />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          name="title"
+          content="An Outstanding Legal Practitioner | Attorney Extra-Ordinaire"
+        />
+        <meta
+          name="keywords"
+          content="legal services, law firm, attorney, lawyer, legal advice, litigation, dispute resolution, contract law, business law, corporate law, employment law, labor law, civil litigation, criminal law, family law, estate planning, real estate law, immigration law, intellectual property law, mediation and arbitration"
+        />
+        <meta
+          property="og:title"
+          content="Hannah Aghedo | An Outstanding Legal Practitioner"
+        />
+        <meta property="og:url" content="https://hannahaghedo.com/#about" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://www.hannahaghedo.com/images/general/hannah-picture.jpg"
+        ></meta>
+        <meta
+          property="og:description"
+          content="Hannah Aghedo is an experienced and dedicated lawyer who is committed to providing high-quality legal services to her clients. With 10+ years of experience in several areas of law, she has a proven track record of success in handling complex legal matters. Hannah Aghedo takes a personalized approach to every case, working closely with her clients to understand their unique needs and goals. Whether you are facing a legal dispute or need guidance on a business or personal matter, Hannah Aghedo is here to help you achieve the best possible outcome."
+        ></meta>
+        <meta property="og:locale" content="en_US"></meta>
+        <meta
+          property="twitter:title"
+          content="Hannah Aghedo | Outstanding Legal Practitioner"
+        ></meta>
+        <meta
+          property="twitter:description"
+          content="Hannah Aghedo is an experienced and dedicated lawyer who is committed to providing high-quality legal services to her clients. With 10+ years of experience in several areas of law, she has a proven track record of success in handling complex legal matters. Hannah Aghedo takes a personalized approach to every case, working closely with her clients to understand their unique needs and goals. Whether you are facing a legal dispute or need guidance on a business or personal matter, Hannah Aghedo is here to help you achieve the best possible outcome."
+        ></meta>
+        <meta
+          property="twitter:image"
+          content="https://www.hannahaghedo.com/images/general/hannah-picture.jpg"
+        ></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           rel="apple-touch-icon"
@@ -249,7 +284,7 @@ export default function Contact({ data, services }) {
       </main>
       {submitted && (
         <div className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center bg-gray-900 bg-opacity-70 z-50">
-          <div className="bg-tuatara text-white rounded-lg p-8 space-y-4">
+          <div className="bg-tuatara text-white rounded-lg m-8 p-8 space-y-4">
             <p className="text-2xl">Thanks for contacting us!</p>
             <p className="text-2xl">We will give you a feedback shortly</p>
             <div className="flex justify-center space-x-4">
